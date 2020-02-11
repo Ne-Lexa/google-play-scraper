dad409033f00142e5ff9619220a0c8f8dcf45679
Ne-Lexa <alexey@nelexa.ru>
2020-02-11 10:43:05 +0300

    modify lazy stream

--- src/Model/LazyStream.php
+++ src/Model/LazyStream.php
@@ -4,16 +4,38 @@ declare(strict_types=1);
 
 namespace Nelexa\GPlay\Model;
 
-use GuzzleHttp\Psr7\LazyOpenStream;
+use GuzzleHttp\Psr7\StreamDecoratorTrait;
 use Psr\Http\Message\StreamInterface;
 
+use function GuzzleHttp\Psr7\stream_for;
+use function GuzzleHttp\Psr7\try_fopen;
+
 /**
- * Class LazyStream.
+ * Lazily reads or writes to a file that is opened only after an IO operation
+ * take place on the stream.
  *
  * @internal
  */
-class LazyStream extends LazyOpenStream
+final class LazyStream implements StreamInterface
 {
+    use StreamDecoratorTrait;
+
+    /** @var string File to open */
+    private $filename;
+
+    /** @var string $mode */
+    private $mode;
+
+    /**
+     * @param string $filename File to lazily open
+     * @param string $mode     fopen mode to use when opening the stream
+     */
+    public function __construct(string $filename, string $mode)
+    {
+        $this->filename = $filename;
+        $this->mode = $mode;
+    }
+
     /**
      * @param string $from
      * @param string $to
@@ -22,7 +44,6 @@ class LazyStream extends LazyOpenStream
     {
         $this->filename = str_replace($from, $to, $this->filename);
     }
-
     /**
      * @return string
      */
@@ -40,16 +61,18 @@ class LazyStream extends LazyOpenStream
     }
 
     /**
+     * Creates the underlying stream lazily when required.
+     *
      * @return StreamInterface
      */
     protected function createStream()
     {
-        $dir = \dirname($this->getFilename());
+        $dir = \dirname($this->filename);
 
         if (!is_dir($dir) && !mkdir($dir, 0755, true) && !is_dir($dir)) {
             throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
         }
 
-        return parent::createStream();
+        return stream_for(try_fopen($this->filename, $this->mode));
     }
 }
